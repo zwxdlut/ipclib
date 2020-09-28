101e7305e07841798daf35ed3b7fd899d79bfd32
zwx <zwx@meixing.com>
2020-09-28 14:17:29 +0800

    Rename modules and variables.

--- test/main_client.cc
+++ test/main_client.cc
@@ -23,20 +23,20 @@ int main(int argc, char** argv)
     double f64 = 100.1;
     uint8_t i8 = 0;
     myservice_callback callback;
-    auto proxy = myservice_factory::create(myservice_factory::interface_type::PROXY);
+    auto service = myservice_factory::create(myservice_factory::interface_type::PROXY);
 
-    proxy->add_callback(&callback);
+    service->add_callback(&callback);
 
-    proxy->func1(i32, b, a, va );
+    service->func1(i32, b, a, va );
     LOGI(TAG, "func1 <<< %d, %d, %s, %s", i32, b, formater::format(a)->c_str(), formater::format(va )->c_str());
 
-    proxy->func2(f64);
+    service->func2(f64);
     LOGI(TAG, "func2 <<< %lf", f64);
 
-    proxy->func3(i8);
+    service->func3(i8);
     LOGI(TAG, "func3 <<< %d", i8);
 
-    proxy->func4();
+    service->func4();
     LOGI(TAG, "func4 <<<");
 
     while(1)
