0c164e85702f9ddf201c1bec1bf53ce0748c473c
zwxdlut <zwxdlut@163.com>
2020-12-28 14:01:48 +0800

    Modify build scripts.

--- envsetup.sh
+++ envsetup.sh
@@ -1,41 +1,10 @@
 #!/bin/bash
 
-#设置编译环境变量
+# setup build environment
 function setup_env()
 {
-	PLATFORM=-1
-	
-	echo
-    echo "platform supported："
-    echo 
-    echo 0:x86_64-linux-gnu
-    echo 
-    echo 1:raspberry pi
-    echo 
-    
-    until [ "$PLATFORM" = "0" ] || 
-          [ "$PLATFORM" = "1" ]
-		  
-    do
-        read -p "select platform：" PLATFORM
-    done
-    
-    case $PLATFORM in
-    0)
-        ;;
-    1)
-        export SYS_ROOT=/opt/rpi/root
-        export CROSS_COMPILE=/opt/rpi/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf- 
-        export CC=$CROSS_COMPILE"gcc"
-        export CXX=$CROSS_COMPILE"g++"
-        export AR=$CROSS_COMPILE"ar"
-        ;; 
-    esac
-
-    export PLATFORM
-
-    IPC_SCHEME=-1
-    
+    # ipc scheme
+    IPC_SCHEME_NO=-1
     echo
     echo "ipc scheme："
     echo 
@@ -44,14 +13,52 @@ function setup_env()
     echo 1:rpclib
     echo 
     
-    until [ "$IPC_SCHEME" = "0" ] || 
-          [ "$IPC_SCHEME" = "1" ]
+    until [ "$IPC_SCHEME_NO" = "0" ] || 
+          [ "$IPC_SCHEME_NO" = "1" ]
           
     do
-        read -p "please select ipc scheme：" IPC_SCHEME
+        read -p "please select ipc scheme：" IPC_SCHEME_NO
     done
 
-    export IPC_SCHEME
+    case $IPC_SCHEME_NO in
+    0)
+        export IPC_SCHEME="dbus"
+        ;;
+    1)
+        export IPC_SCHEME="rpclib"
+        ;; 
+    esac
+
+    # platform
+	PLATFORM_NO=-1
+	echo
+    echo "platform supported："
+    echo 
+    echo 0:x86_64-linux-gnu
+    echo 
+    echo 1:raspberry-pi
+    echo 
+
+    until [ "$PLATFORM_NO" = "0" ] || 
+          [ "$PLATFORM_NO" = "1" ]
+		  
+    do
+        read -p "select platform：" PLATFORM_NO
+    done
+    
+    case $PLATFORM_NO in
+    0)
+        export PLATFORM="x86_64-linux-gnu"
+        ;;
+    1)
+        export PLATFORM="raspberry-pi"
+        export SYS_ROOT=/opt/rpi/root
+        export CROSS_COMPILE=/opt/rpi/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf- 
+        export CC=$CROSS_COMPILE"gcc"
+        export CXX=$CROSS_COMPILE"g++"
+        export AR=$CROSS_COMPILE"ar"
+        ;; 
+    esac
 
     pwd
 	
